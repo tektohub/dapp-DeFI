cef694ed64f231ae9561482f44aba8fde507f3f2
MB2M <damsbooster@gmail.com>
2021-12-03 23:01:16 +0100

    change dev addr

--- migrations/3_deploy_masterchef.js
+++ migrations/3_deploy_masterchef.js
@@ -1,7 +1,7 @@
 var Masterchef = artifacts.require("./MasterChef/MasterChef.sol");
 var MCTOToken = artifacts.require("./MasterChef/MCTO.sol");
 
-module.exports = function (deployer) {
-  // deployer.deploy(MCTOToken).then( (instance) => deployer.deploy(Masterchef,
-  //   instance.address, 0, 10, 0, 1000));
+module.exports = function(deployer) {
+  deployer.deploy(MCTOToken).then( (instance) => deployer.deploy(Masterchef,
+    instance.address, '0xd26c2c8195e1ecb22Fd9a3227A98de3e31cE943a', 10, 0, 1000));
 };
