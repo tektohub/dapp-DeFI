a7beb8b0c83c4226dfa86a073166e64e283d3547
Fabien F <dakeyras.crypto@gmail.com>
2021-12-05 23:29:27 +0100

    update mamieswap script

--- migrations/2_deploy_mamiecryptoswap.js
+++ migrations/2_deploy_mamiecryptoswap.js
@@ -1,4 +1,4 @@
-const {BN, constants} = require("@openzeppelin/test-helpers");
+const { BN, constants } = require("@openzeppelin/test-helpers");
 
 const MamieCryptoV2Factory = artifacts.require("MamieCryptoV2Factory");
 // const MamieCryptoV2Pair = artifacts.require("MamieCryptoV2Pair");
@@ -8,50 +8,51 @@ const fUSDT = artifacts.require("fUSDT");
 const fDAI = artifacts.require("fDAI");
 const WETH = artifacts.require("WETH9");
 
-const oneMillion = new BN(web3.utils.toWei("1000000", "ether"))
+const oneMillion = new BN(web3.utils.toWei("1000000", "ether"));
 
-module.exports = async function(deployer, network, addresses) {
-    await deployer.deploy(fUSDC, oneMillion);
-    await deployer.deploy(fUSDT, oneMillion);
-    await deployer.deploy(fDAI, oneMillion);
-    await deployer.deploy(WETH);
+module.exports = async function (deployer, network, addresses) {
+  await deployer.deploy(fUSDC, oneMillion);
+  await deployer.deploy(fUSDT, oneMillion);
+  await deployer.deploy(fDAI, oneMillion);
+  await deployer.deploy(WETH);
 
-    const fUSDCContract = await fUSDC.deployed()
-    const fUSDTContract = await fUSDT.deployed()
-    const fDAIContract = await fDAI.deployed()
-    const WETHCContract = await WETH.deployed()
+  // const fUSDCContract = await fUSDC.deployed()
+  // const fUSDTContract = await fUSDT.deployed()
+  // const fDAIContract = await fDAI.deployed()
+  const WETHCContract = await WETH.deployed();
 
-    await deployer.deploy(MamieCryptoV2Factory, addresses[0] )
-    factoryContract = await MamieCryptoV2Factory.deployed()
+  await deployer.deploy(MamieCryptoV2Factory, addresses[0]);
+  factoryContract = await MamieCryptoV2Factory.deployed();
 
-    await deployer.deploy(MamieCryptoV2Router02, factoryContract.address, WETHCContract.address)
-    routerContract = await MamieCryptoV2Router02.deployed()
+  await deployer.deploy(
+    MamieCryptoV2Router02,
+    factoryContract.address,
+    WETHCContract.address
+  );
+  routerContract = await MamieCryptoV2Router02.deployed();
 
-    // await factoryContract.createPair(fUSDCContract.address, WETHCContract.address);
-    // usdcPairAddress = await factoryContract.getPair(fUSDCContract.address, WETHCContract.address);
-    // console.log(`USDC pair:${usdcPairAddress}`);
+  // await factoryContract.createPair(fUSDCContract.address, WETHCContract.address);
+  // usdcPairAddress = await factoryContract.getPair(fUSDCContract.address, WETHCContract.address);
+  // console.log(`USDC pair:${usdcPairAddress}`);
 
-    // await factoryContract.createPair(fUSDTContract.address, WETHCContract.address);
-    // usdtPairAddress = await factoryContract.getPair(fUSDTContract.address, WETHCContract.address);
-    // console.log(`USDT pair:${usdtPairAddress}`);
+  // await factoryContract.createPair(fUSDTContract.address, WETHCContract.address);
+  // usdtPairAddress = await factoryContract.getPair(fUSDTContract.address, WETHCContract.address);
+  // console.log(`USDT pair:${usdtPairAddress}`);
 
-    // await factoryContract.createPair(fDAIContract.address, WETHCContract.address);
-    // daiPairAddress = await factoryContract.getPair(fDAIContract.address, WETHCContract.address);
-    // console.log(`DAI pair:${daiPairAddress}`);
+  // await factoryContract.createPair(fDAIContract.address, WETHCContract.address);
+  // daiPairAddress = await factoryContract.getPair(fDAIContract.address, WETHCContract.address);
+  // console.log(`DAI pair:${daiPairAddress}`);
 
+  // await fUSDCContract.approve(routerContract.address, oneMillion, {
+  //     from: addresses[0],
+  //   });
 
-    // await fUSDCContract.approve(routerContract.address, oneMillion, {
-    //     from: addresses[0],
-    //   });
-
-
-    // await routerContract.addLiquidityETH(
-    //     fUSDCContract.address, 
-    //     new BN(100000),
-    //     0,
-    //     0,
-    //     addresses[0],
-    //     constants.MAX_UINT256, {value: new BN(100000)}
-    // )
-
-};  
+  // await routerContract.addLiquidityETH(
+  //     fUSDCContract.address,
+  //     new BN(100000),
+  //     0,
+  //     0,
+  //     addresses[0],
+  //     constants.MAX_UINT256, {value: new BN(100000)}
+  // )
+};
--- script/1_mamieswap.js
+++ script/1_mamieswap.js
@@ -1,5 +1,5 @@
-const Web3 = require('web3');
-const {BN, constants} = require("@openzeppelin/test-helpers");
+const Web3 = require("web3");
+const { BN, constants } = require("@openzeppelin/test-helpers");
 const MamieCryptoV2Factory = artifacts.require("MamieCryptoV2Factory");
 const MamieCryptoV2Router02 = artifacts.require("MamieCryptoV2Router02");
 const MamieCryptoV2Pair = artifacts.require("MamieCryptoV2Pair");
@@ -10,14 +10,12 @@ const WETH9 = artifacts.require("WETH9");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 require("dotenv").config();
 
-const oneMillion = new BN(web3.utils.toWei("1000000", "ether"))
+const oneMillion = new BN(web3.utils.toWei("1000000", "ether"));
 
 const getWeb3 = () => {
-  return new Web3(new HDWalletProvider(
-    `${process.env.MNEMONIC}`,
-    `http://127.0.0.1:8545`,
-    0
-  ));
+  return new Web3(
+    new HDWalletProvider(`${process.env.MNEMONIC}`, `http://127.0.0.1:8545`, 0)
+  );
 };
 
 const script = async () => {
@@ -27,6 +25,7 @@ const script = async () => {
   const deployedNetwork = MamieCryptoV2Factory.networks[networkId];
   const accounts = await web3.eth.getAccounts();
 
+  // ######## Init contracts objects ########
   const factory = new web3.eth.Contract(
     MamieCryptoV2Factory.abi,
     MamieCryptoV2Factory.networks[networkId].address
@@ -56,103 +55,184 @@ const script = async () => {
     WETH9.abi,
     WETH9.networks[networkId].address
   );
+  console.log(`############# Contracts Address ##########`);
+  console.log(`Factory :${factory._address}`);
+  console.log(`Router02 :${router._address}`);
+  console.log(`fUSDC :${fUSDCContract._address}`);
+  console.log(`fUSDT :${fUSDTContract._address}`);
+  console.log(`fDAI :${fDAIContract._address}`);
+  console.log(`WETH :${WETHCContract._address}`);
 
-  //Create Pairs
-  await factory.methods
-    .createPair(fUSDCContract._address, WETHCContract._address)
-    .send({from: accounts[0]});
+  // ######## Create Pairs ########
+
+  // USDC
   usdcPairAddress = await factory.methods
     .getPair(fUSDCContract._address, WETHCContract._address)
     .call();
-  console.log(`USDC pair:${usdcPairAddress}`);
-  const pair0 = new web3.eth.Contract(
-    MamieCryptoV2Pair.abi,
-    usdcPairAddress
-  );
 
-  await factory.methods
-    .createPair(fUSDTContract._address, WETHCContract._address)
-    .send({from: accounts[0]});
+  if (usdcPairAddress === constants.ZERO_ADDRESS) {
+    await factory.methods
+      .createPair(fUSDCContract._address, WETHCContract._address)
+      .send({ from: accounts[0] });
+    usdcPairAddress = await factory.methods
+      .getPair(fUSDCContract._address, WETHCContract._address)
+      .call();
+  }
+
+  const pair0 = new web3.eth.Contract(MamieCryptoV2Pair.abi, usdcPairAddress);
+
+  // USDT
   usdtPairAddress = await factory.methods
     .getPair(fUSDTContract._address, WETHCContract._address)
     .call();
-  console.log(`USDT pair:${usdtPairAddress}`);
-  const pair1 = new web3.eth.Contract(
-    MamieCryptoV2Pair.abi,
-    usdtPairAddress
-  );
 
-  await factory.methods
-    .createPair(fDAIContract._address, WETHCContract._address)
-    .send({from: accounts[0]});
+  if (usdtPairAddress === constants.ZERO_ADDRESS) {
+    await factory.methods
+      .createPair(fUSDTContract._address, WETHCContract._address)
+      .send({ from: accounts[0] });
+    usdtPairAddress = await factory.methods
+      .getPair(fUSDTContract._address, WETHCContract._address)
+      .call();
+  }
+
+  const pair1 = new web3.eth.Contract(MamieCryptoV2Pair.abi, usdtPairAddress);
+
+  // DAI
   daiPairAddress = await factory.methods
     .getPair(fDAIContract._address, WETHCContract._address)
     .call();
+
+  if (daiPairAddress === constants.ZERO_ADDRESS) {
+    await factory.methods
+      .createPair(fDAIContract._address, WETHCContract._address)
+      .send({ from: accounts[0] });
+    daiPairAddress = await factory.methods
+      .getPair(fDAIContract._address, WETHCContract._address)
+      .call();
+  }
+
+  const pair2 = new web3.eth.Contract(MamieCryptoV2Pair.abi, daiPairAddress);
+
+  console.log(`############# Pair address ##########`);
+  console.log(`USDC pair:${usdcPairAddress}`);
+  console.log(`USDT pair:${usdtPairAddress}`);
   console.log(`DAI pair:${daiPairAddress}`);
-  const pair2 = new web3.eth.Contract(
-    MamieCryptoV2Pair.abi,
-    daiPairAddress
-  );
 
   //approvals
   await fUSDCContract.methods
     .approve(router._address, new BN(oneMillion))
-    .send({from: accounts[0]});
+    .send({ from: accounts[0] });
   await fUSDTContract.methods
     .approve(router._address, new BN(oneMillion))
-    .send({from: accounts[0]});
+    .send({ from: accounts[0] });
   await fDAIContract.methods
     .approve(router._address, new BN(oneMillion))
-    .send({from: accounts[0]});
+    .send({ from: accounts[0] });
+
+  console.log(`############# Approve 1 million for each token`);
+
+  console.log(
+    `############# Adding liquidities. 10 ETH for 40000$ (4000$/ETH) ##############`
+  );
 
   //Add liquidities
   await router.methods
     .addLiquidityETH(
-      fUSDCContract._address, 
-      new BN(100000),
+      fUSDCContract._address,
+      new BN(web3.utils.toWei("40000")),
       0,
       0,
       accounts[0],
-      constants.MAX_UINT256)
-    .send({from: accounts[0], value: new BN(200000)});
-  
-    let r = await pair0.methods.getReserves().call()
-    console.log('PAIR USDC/ETH:')
-    console.log(`Reserve token 0: ${r._reserve0}`)
-    console.log(`Reserve token 1: ${r._reserve1}`)
+      constants.MAX_UINT256
+    )
+    .send({ from: accounts[0], value: new BN(web3.utils.toWei("10")) });
+
+  let r = await pair0.methods.getReserves().call();
+  let pair0_reserve0, pair0_reserve1;
+  pair0_reserve0 = r._reserve0;
+  pair0_reserve1 = r._reserve1;
+  //console.log(b);
+
+  console.log("#### PAIR USDC/ETH");
+  console.log(`Reserve token 0: ${web3.utils.fromWei(pair0_reserve0)}`);
+  console.log(`Reserve token 1: ${web3.utils.fromWei(pair0_reserve1)}`);
+  console.log(
+    `lpTokens in wallet: ${web3.utils.fromWei(
+      await pair0.methods.balanceOf(accounts[0]).call()
+    )}`
+  );
 
   await router.methods
     .addLiquidityETH(
-      fUSDTContract._address, 
-      new BN(100000),
+      fUSDTContract._address,
+      new BN(web3.utils.toWei("40000")),
       0,
       0,
       accounts[0],
-      constants.MAX_UINT256)
-    .send({from: accounts[0], value: new BN(300000)});
-  
-    r = await pair1.methods.getReserves().call()
-    console.log('PAIR USDT/ETH:')
-    console.log(`Reserve token 0: ${r._reserve0}`)
-    console.log(`Reserve token 1: ${r._reserve1}`)
-  
+      constants.MAX_UINT256
+    )
+    .send({ from: accounts[0], value: new BN(web3.utils.toWei("10")) });
+
+  r = await pair1.methods.getReserves().call();
+
+  console.log("#### PAIR USDT/ETH:");
+  console.log(`Reserve token 0: ${web3.utils.fromWei(r._reserve0)}`);
+  console.log(`Reserve token 1: ${web3.utils.fromWei(r._reserve1)}`);
+  console.log(
+    `lpTokens in wallet: ${web3.utils.fromWei(
+      await pair1.methods.balanceOf(accounts[0]).call()
+    )}`
+  );
+
   await router.methods
     .addLiquidityETH(
-      fDAIContract._address, 
-      new BN(100000),
+      fDAIContract._address,
+      new BN(web3.utils.toWei("40000")),
       0,
       0,
       accounts[0],
-      constants.MAX_UINT256)
-    .send({from: accounts[0], value: new BN(400000)});
-  
-    r = await pair2.methods.getReserves().call()
-    console.log('PAIR DAI/ETH:')
-    console.log(`Reserve token 0: ${r._reserve0}`)
-    console.log(`Reserve token 1: ${r._reserve1}`)
-  
+      constants.MAX_UINT256
+    )
+    .send({ from: accounts[0], value: new BN(web3.utils.toWei("10")) });
+
+  r = await pair2.methods.getReserves().call();
+  //b = await pair2.balanceOf(accounts[0]);
+
+  console.log("#### PAIR DAI/ETH:");
+  console.log(`Reserve token 0: ${web3.utils.fromWei(r._reserve0)}`);
+  console.log(`Reserve token 1: ${web3.utils.fromWei(r._reserve1)}`);
+  console.log(
+    `lpTokens in wallet: ${web3.utils.fromWei(
+      await pair2.methods.balanceOf(accounts[0]).call()
+    )}`
+  );
+
+  console.log(`############# Swap 1 ETH for USDC ##############`);
+  let amountOut = await router.methods
+    .getAmountOut(new BN(web3.utils.toWei("1")), pair0_reserve0, pair0_reserve1)
+    .call();
+  console.log(`AmountOut (USDC):${web3.utils.fromWei(amountOut)}`);
+  await router.methods
+    .swapETHForExactTokens(
+      amountOut, // amountOut
+      [WETHCContract._address, fUSDCContract._address], // path
+      accounts[0], // to
+      constants.MAX_UINT256 // deadline
+    )
+    .send({ from: accounts[0], value: new BN(web3.utils.toWei("1")) });
+  console.log("Swap done !");
+
+  console.log(
+    `############# Checking reserves for WETH/USDC pool ##############`
+  );
+  r = await pair0.methods.getReserves().call();
+  pair0_reserve0 = r._reserve0;
+  pair0_reserve1 = r._reserve1;
+
+  console.log(`Reserve token 0: ${web3.utils.fromWei(pair0_reserve0)}`);
+  console.log(`Reserve token 1: ${web3.utils.fromWei(pair0_reserve1)}`);
 };
 
 module.exports = () => {
-  script()
-}
+  script();
+};
