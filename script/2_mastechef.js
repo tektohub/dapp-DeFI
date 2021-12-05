e9b1af0ece312a6b9dae8e3d0c4d8d53bde786ab
MB2M <damsbooster@gmail.com>
2021-12-05 13:56:13 +0100

    update masterchef script

--- script/2_mastechef.js
+++ script/2_mastechef.js
@@ -1,21 +1,25 @@
-import getWeb3 from "./getWeb3";
-import MasterChefContract from "../build/contracts/MasterChef.json";
-import MCTOContract from "../build/contracts/MCTO.json";
+const Web3 = require('web3');
+const MasterChefContract = require('../build/contracts/MasterChef.json')
+const MCTOContract = require('../build/contracts/MCTO.json')
+// import MasterChefContract from "../build/contracts/MasterChef.json";
+// import MCTOContract from "../build/contracts/MCTO.json";
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 require("dotenv").config();
 
-LP_fUSDC_WETH_address = "";
-LP_fUSDT_WETH_address = "";
-LP_fDAI_WETH_address = "";
+LP_fUSDC_WETH_address = "0xa83E7bA00C98BcF23Bf1DcE94559825f5B6DB76A";
+LP_fUSDT_WETH_address = "0x8118f142Dff3a95dde31dBbbf25803a1dec960c2";
+LP_fDAI_WETH_address = "0x90B7525dB4f3174fC3AC98369a66D800E8aD616B";
 
 const getWeb3 = () => {
-  return new HDWalletProvider(
+  return new Web3(new HDWalletProvider(
     `${process.env.MNEMONIC}`,
-    `http://127.0.0.1:8545`
-  );
+    `http://127.0.0.1:8545`,
+    0
+  ));
 };
 
 const script = async () => {
+  // const web3 = getWeb3();
   const web3 = getWeb3();
   const networkId = await web3.eth.net.getId();
   const deployedNetwork = MasterChefContract.networks[networkId];
@@ -41,3 +45,8 @@ const script = async () => {
     .add(5000, LP_fDAI_WETH_address, true)
     .send({ from: accounts[0] });
 };
+
+module.exports = () => {
+
+  script()
+}
